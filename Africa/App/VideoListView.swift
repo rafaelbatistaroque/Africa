2b376354e8729dff1bd4cb0a645382ed239c7148
Rafael Batista <maranata_rafa@hotmail.com>
2022-09-13 21:06:05 -0400

    create video player with swiftUI pt-2

--- Africa/App/VideoListView.swift
+++ Africa/App/VideoListView.swift
@@ -9,8 +9,10 @@ struct VideoListView: View {
         NavigationView {
             List {
                 ForEach(videos) { video in
-                    VideoListItemView(video: video)
-                        .padding(.vertical, 8)
+                    NavigationLink(destination: VideoPlayerView(videoSelected: video.id, videoTitle: video.name)) {
+                        VideoListItemView(video: video)
+                            .padding(.vertical, 8)
+                    }
                 }
             }
             .listStyle(InsetGroupedListStyle())
---
+++ Africa/View/VideoPlayerView.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+import AVKit
+
+struct VideoPlayerView: View {
+    var videoSelected: String
+    var videoTitle: String
+
+    var body: some View {
+        VStack {
+            VideoPlayer(player: playVideo(fileName: videoSelected, fileFormat: "mp4"))
+            .overlay(
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 32, height: 32)
+                    .padding(.top, 6)
+                    .padding(.horizontal, 8),
+                alignment: .topLeading
+            )
+        }
+        .accentColor(.accentColor)
+        .navigationTitle(videoTitle)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct VideoPlayerView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoPlayerView(videoSelected: "lion", videoTitle: "Lion")
+    }
+}
