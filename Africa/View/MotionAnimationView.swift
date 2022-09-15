fe0a43ef093ac6a0b7a8b08e377fd657dc36a727
Rafael Batista <maranata_rafa@hotmail.com>
2022-09-15 07:21:36 -0400

    create motion animation view

---
+++ Africa/View/MotionAnimationView.swift
@@ -0,0 +1,59 @@
+import SwiftUI
+
+struct MotionAnimationView: View {
+    @State private var randomCircle = Int.random(in: 12...16)
+    @State private var isAnimating: Bool = false
+
+    func randomCoordinate(max: CGFloat) -> CGFloat {
+        CGFloat.random(in: 0...max)
+    }
+
+    func randomSize() -> CGFloat {
+        CGFloat(Int.random(in: 10...300))
+    }
+
+    func randomScale() -> CGFloat {
+        CGFloat(Double.random(in: 0.1...2.0))
+    }
+
+    func randomSpeed() -> Double {
+        Double.random(in: 0.025...1.0)
+    }
+
+    func randomDelay() -> Double {
+        Double.random(in: 0...2)
+    }
+
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                ForEach(0...randomCircle, id: \.self) { item in
+                    Circle()
+                        .foregroundColor(.gray)
+                        .opacity(0.15)
+                        .frame(width: randomSize(), height: randomSize(), alignment: .center)
+                        .scaleEffect(isAnimating ? randomScale() : 1)
+                        .position(
+                            x: randomCoordinate(max: geometry.size.width),
+                            y: randomCoordinate(max: geometry.size.height))
+                        .animation(
+                            .interpolatingSpring(stiffness: 0.5, damping: 0.5)
+                            .repeatForever()
+                            .speed(randomSpeed())
+                            .delay(randomDelay())
+                            , value: UUID())
+                        .onAppear(perform: {
+                            isAnimating = true
+                        })
+                }
+            }
+            .drawingGroup()
+        }
+    }
+}
+
+struct MotionAnimationView_Previews: PreviewProvider {
+    static var previews: some View {
+        MotionAnimationView()
+    }
+}
