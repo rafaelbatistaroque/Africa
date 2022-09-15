7c28e52fbecc8e7a106a603806b80c7e903b2956
Rafael Batista <maranata_rafa@hotmail.com>
2022-09-14 20:56:42 -0400

    add Map with custom annotations - pt 1

--- Africa/App/MapView.swift
+++ Africa/App/MapView.swift
@@ -1,8 +1,29 @@
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+
+    @State private var region: MKCoordinateRegion = {
+        var mapCoordinates = CLLocationCoordinate2D(latitude: 6.600286, longitude:16.4377599)
+        var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+
+        return MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+    }()
+
+    let locations: [NationalParkLocation] = Bundle.main.toModel("locations.json")
+
     var body: some View {
-        Text("Map")
+        Map(coordinateRegion: $region, annotationItems: locations, annotationContent: { item in
+//            MapPin(coordinate: item.location, tint: .accentColor)
+//            MapMarker(coordinate: item.location, tint: .accentColor)
+
+            MapAnnotation(coordinate: item.location) {
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 32, height: 32, alignment: .center)
+            }
+        })
     }
 }
 
